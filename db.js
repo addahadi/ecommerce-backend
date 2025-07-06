54a1469ad47a8fbeeb67b2cb6bc1702e84ddffda
addahadi <addajs48@gmail.com>
2025-07-06 16:21:01 +0100

    database changes

--- db.js
+++ db.js
@@ -1,25 +1,11 @@
 const mysql = require("mysql2");
 
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
-
-
-
-const db = mysql.createConnection({
-    host : "127.0.0.1",
-    user : "root",
-    password : "missoumadda",
-    database : "ecommerce-schema"
-})
-
-
-db.connect((err) => {
-    if(err){
-        console.error(err);
-        return;
-    }
-    console.log("connected to mysql successfully")
-})
-
-module.exports = db
-
-
+module.exports = db;
